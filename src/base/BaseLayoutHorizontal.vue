11243994a1775e0fe2b9f89bf6ec9026ee1c7833
BryanAdamss <BryanAdamss@foxmail.com>
2018-09-08 19:38:56 +0800

    [feat] - 添加基础布局组件

---
+++ src/base/BaseLayoutHorizontal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="c-BaseLayoutHorizontal">
+    <div class="c-BaseLayoutHorizontal-side"
+         v-if="this.$slots.side">
+      <slot name="side"></slot>
+    </div>
+    <div class="c-BaseLayoutHorizontal-main">
+      <slot></slot>
+    </div>
+    <div class="c-BaseLayoutHorizontal-extra"
+         v-if="this.$slots.extra">
+      <slot name="extra"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * * 常见水平三列布局
+ */
+export default {
+  name: 'BaseLayoutHorizontal'
+}
+</script>
+
+<style scoped>
+.c-BaseLayoutHorizontal {
+  display: flex;
+  flex-direction: row;
+}
+.c-BaseLayoutHorizontal-side {
+  flex: 0 0 auto;
+}
+.c-BaseLayoutHorizontal-main {
+  flex: 1 1 auto;
+}
+.c-BaseLayoutHorizontal-extra {
+  flex: 0 0 auto;
+}
+</style>
---
+++ src/base/BaseLayoutVertical.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="c-BaseLayoutVertical">
+    <div class="c-BaseLayoutVertical-hd"
+         v-if="this.$slots.header">
+      <slot name="header"></slot>
+    </div>
+    <div class="c-BaseLayoutVertical-bd">
+      <slot></slot>
+    </div>
+    <div class="c-BaseLayoutVertical-ft"
+         v-if="this.$slots.footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * * 常见垂直上中下三行布局
+ */
+export default {
+  name: 'BaseLayoutVertical'
+}
+</script>
+
+<style scoped>
+.c-BaseLayoutVertical {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.c-BaseLayoutVertical-hd {
+  flex: 0 0 auto;
+}
+.c-BaseLayoutVertical-bd {
+  flex: 1 1 auto;
+  height: 100%;
+  overflow: auto;
+  -webkit-overflow-scrolling: touch;
+}
+.c-BaseLayoutVertical-ft {
+  flex: 0 0 auto;
+}
+</style>

eea368498d020184308aff6a88bf3da6c1a1dcca
BryanAdamss <BryanAdamss@foxmail.com>
2018-09-08 19:50:00 +0800

    [feat] - 添加vuex测试组件

---
+++ src/views/VuexTest/VuexTest.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="c-VuexTest">
+    <span>{{testObj.name}}</span>
+    <span @click="change">点击测试vuex</span>
+  </div>
+</template>
+
+<script>
+/**
+ * * VuexTest
+ */
+import { mapGetters, mapMutations } from 'vuex'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    ...mapGetters(['testObj'])
+  },
+  methods: {
+    change() {
+      this.setTestObj({
+        name: `test ${this.count++}`
+      })
+    },
+    ...mapMutations({
+      setTestObj: 'SET_TEST_OBJ'
+    })
+  }
+}
+</script>

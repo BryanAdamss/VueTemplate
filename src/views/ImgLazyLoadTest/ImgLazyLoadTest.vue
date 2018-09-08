bd68d13fd9f024cb2ae6f5515c47afb85df3045d
BryanAdamss <BryanAdamss@foxmail.com>
2018-09-08 19:49:28 +0800

    [feat] - 添加imgLazyload测试组件

---
+++ src/views/ImgLazyLoadTest/ImgLazyLoadTest.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="c-ImgLazyLoadTest">
+    <div class="c-ImgBox">
+      <img v-for="n in 50"
+           :key="n"
+           v-lazy="`http://placehold.it/200x${n+1}0`">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImgLazyLoadTest'
+}
+</script>
+
+<style scoped>
+.c-ImgBox {
+  height: 500px;
+  border: 1px solid blue;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
+</style>
+
+
+ 

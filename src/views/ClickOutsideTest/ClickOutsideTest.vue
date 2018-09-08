0f86af82552dec8f252c823516be430cf7540265
BryanAdamss <BryanAdamss@foxmail.com>
2018-09-08 19:48:20 +0800

    [feat] - 添加clickOutside测试组件

---
+++ src/views/ClickOutsideTest/ClickOutsideTest.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="c-ClickOutsideTest">
+    <div class="c-Box"
+         v-clickoutside="onClickOutside"
+         ref="clickOutsideEl">只有点我外侧才会触发clickOutside指令</div>
+  </div>
+</template>
+<script>
+/**
+ * * ClickOutsideTest
+ */
+
+// * clickOutSide指令
+import clickoutside from '@/directives/click-outside'
+
+export default {
+  name: 'ClickOutsideTest',
+  methods: {
+    onClickOutside(e) {
+      console.log('onClickOutside', e)
+    }
+  },
+  directives: {
+    clickoutside
+  }
+}
+</script>
+<style>
+.c-Box {
+  font-size: 16px;
+  width: 100px;
+  height: 100px;
+  background-color: #f00;
+}
+</style>
+
+
+
+

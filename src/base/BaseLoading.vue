abbdb68f37d60879f98cd8820465ac1b88af572c
BryanAdamss <BryanAdamss@foxmail.com>
2018-09-10 18:04:09 +0800

    [fix] - 添加loading基础组件

---
+++ src/base/BaseLoading.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="c-Loading">
+    <BaseLoadingSpinner :size="spinnerSize"></BaseLoadingSpinner>
+    <div class="c-Loading-text"
+         v-text="text"></div>
+  </div>
+</template>
+<script>
+import BaseLoadingSpinner from '@/base/BaseLoadingSpinner'
+
+/**
+ * @description 基础Loading
+ */
+export default {
+  name: 'BaseLoading',
+  props: {
+    text: {
+      type: String,
+      default: '加载中...'
+    },
+    spinnerSize: {
+      type: String,
+      default: '26px'
+    }
+  },
+  components: {
+    BaseLoadingSpinner
+  }
+}
+</script>
+<style scoped>
+.c-Loading {
+  text-align: center;
+}
+.c-Loading-text {
+  font-size: 16px;
+  color: #333;
+}
+</style>
+

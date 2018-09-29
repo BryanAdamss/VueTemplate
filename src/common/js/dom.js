ab541599b65bf3a12fb8f1d8c4fffbf4a4d49753
BryanAdamss <BryanAdamss@foxmail.com>
2018-09-29 14:35:23 +0800

    [feat] - 添加一些通用函数

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,39 +1,39 @@
 // dom.js用来保存通用的dom操作函数
 export function addClass(el, className) {
   if (hasClass(el, className)) {
-    return;
+    return
   }
-  let newClass = el.className.split(' ');
-  newClass.push(className);
-  el.className = newClass.join(' ');
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass.join(' ')
 }
 
 export function removeClass(el, className) {
   if (!hasClass(el, className)) {
-    return;
+    return
   }
-  let newClassList = el.className.split(' ');
-  newClassList.splice(newClassList.indexOf(className), 1);
-  el.className = newClassList.join(' ');
+  let newClassList = el.className.split(' ')
+  newClassList.splice(newClassList.indexOf(className), 1)
+  el.className = newClassList.join(' ')
 }
 
 export function hasClass(el, className) {
-  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)');
-  return reg.test(el.className);
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
 }
 
 export function getData(el, name, val) {
-  const prefix = 'data-';
-  name = prefix + name;
+  const prefix = 'data-'
+  name = prefix + name
   if (val) {
-    return el.setAttribute(name, val);
+    return el.setAttribute(name, val)
   } else {
-    return el.getAttribute(name);
+    return el.getAttribute(name)
   }
 }
 
 // 能力检测，判断浏览器支持哪种前缀
-let elementStyle = document.createElement('div').style;
+let elementStyle = document.createElement('div').style
 // 利用IIFE得到支持的前缀
 let vendor = (() => {
   // 利用transform做能力检测，来判断支持哪种前缀
@@ -43,30 +43,30 @@ let vendor = (() => {
     O: 'OTransform',
     ms: 'msTransform',
     standard: 'transform'
-  };
+  }
 
   for (let key in transformNames) {
     // 支持某种前缀则直接返回
     if (elementStyle[transformNames[key]] !== undefined) {
-      return key;
+      return key
     }
   }
 
   // 如果没有匹配，则返回false
-  return false;
-})();
+  return false
+})()
 
 // 添加前缀
 export function prefixStyle(style) {
   if (vendor === false) {
-    return false;
+    return false
   }
   // 支持标准，则直接返回
   if (vendor === 'standard') {
-    return style;
+    return style
   }
   // 否则返回prefix后的字符串
-  return vendor + style.charAt(0).toUpperCase() + style.substr(1);
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
 }
 
 // 获取元素尺寸
@@ -86,5 +86,39 @@ export function getRect(el) {
       width: el.offsetWidth,
       height: el.offsetHeight
     }
+  }
+}
+
+/**
+ * 复制到剪贴板
+ */
+export function copyToClipboard(str) {
+  const el = document.createElement('textarea')
+  el.value = str
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.left = '-9999px'
+  document.body.appendChild(el)
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
+  if (selected) {
+    document.getSelection().removeAllRanges()
+    document.getSelection().addRange(selected)
+  }
+}
+
+/**
+ * 滚动到顶部
+ */
+export function scrollToTop() {
+  const c = document.documentElement.scrollTop || document.body.scrollTop
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop)
+    window.scrollTo(0, c - c / 8)
   }
 }
--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -22,24 +22,63 @@ export function shuffle(arr) {
     _arr[i] = _arr[j]
     _arr[j] = t
   }
-  return _arr;
+  return _arr
 }
 
 /**
- * 节流降噪
+ * 去抖函数
+ * 连续触发某一事件时，只在最后一次事件触发时的delay时间后才真正执行处理函数
+ * 场景：搜索建议（停止输入后再触发请求）
  * @param func
  * @param delay
  * @returns {Function}
  */
 export function debounce(func, delay) {
   let timer
-  return function (...args) {
+  delay = delay || 500
+  return function(...args) {
     if (timer) {
       clearTimeout(timer)
     }
     timer = setTimeout(() => {
       func.apply(this, args)
     }, delay)
+  }
+}
+
+/**
+ * 节流函数
+ * 连续触发某一事件时，会自动间隔interval时间去执行一次处理函数
+ * interval时间内重复触发的事件会被忽略
+ * 节流会稀释函数的执行频率
+ * 场景：resize、scroll时并不需要如此密集的事件触发频率
+ * @param func
+ * @param interval
+ * @returns {Function}
+ */
+export function throttle(fn, interval) {
+  var _self = fn, //保存需要被延迟的函数
+    firstTime = true, // 是否首次调用
+    intervalTime = interval || 500, // 间隔调用时间，默认500毫秒
+    timer // 定时器
+  return function() {
+    var args = arguments,
+      _me = this
+    if (firstTime) {
+      // 如果第一次，则无需延迟，直接调用
+      _self.apply(_me, args)
+      return (firstTime = false)
+    }
+    if (timer) {
+      // 如果定时器存在，说明前一次执行还没有完成
+      return false
+    }
+    timer = setTimeout(function() {
+      // 延迟intervalTime后执行
+      clearTimeout(timer)
+      timer = null
+      _self.apply(_me, args)
+    }, intervalTime)
   }
 }
 
@@ -59,41 +98,71 @@ export function debounce(func, delay) {
  * // => [['a', 'b', 'c'], ['d']]
  */
 export function chunk(array, size) {
-  let length = array.length ? array.length : 0;
+  let length = array.length ? array.length : 0
   if (!length || !size) {
-    return [];
+    return []
   }
-  let index = 0;
-  let resIndex = 0;
-  let result = new Array(Math.ceil(length / size));
+  let index = 0
+  let resIndex = 0
+  let result = new Array(Math.ceil(length / size))
 
   while (index < length) {
-    result[resIndex++] = array.slice(index, index += size);
+    result[resIndex++] = array.slice(index, (index += size))
   }
-  return result;
+  return result
 }
 
 /**
  * dateFormat unix时间戳格式化
  */
 export function dateFormat(timestamp, format = 'yyyy-MM-dd hh:mm:ss') {
-  timestamp = timestamp.length === 13 ? timestamp : timestamp * 1000;
+  timestamp = timestamp.length === 13 ? timestamp : timestamp * 1000
   let date = new Date(timestamp)
   let args = {
     'M+': date.getMonth() + 1,
     'd+': date.getDate(),
     'h+': date.getHours(),
     'm+': date.getMinutes(),
-    's+': date.getSeconds(),
-  };
+    's+': date.getSeconds()
+  }
   if (/(y+)/.test(format)) {
-    format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+    format = format.replace(
+      RegExp.$1,
+      (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+    )
   }
   for (var i in args) {
-    let n = args[i];
+    let n = args[i]
     if (new RegExp('(' + i + ')').test(format)) {
-      format = format.replace(RegExp.$1, RegExp.$1.length === 1 ? n : ('00' + n).substr(('' + n).length));
+      format = format.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? n : ('00' + n).substr(('' + n).length)
+      )
     }
   }
-  return format;
+  return format
+}
+
+/**
+ * 判断是否为类数组
+ * @param {*} collection
+ */
+export function isArrayLike(collection) {
+  const MAX_ARRAY_INDEX = Math.pow(2, 53) - 1
+  // 返回参数 collection 的 length 属性值
+  let length = collection.length
+  return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX
+}
+
+/**
+ * 判断数组或对象是否为空
+ * @param {*} obj
+ */
+export function isEmpty(obj) {
+  if (obj == null) return true
+  if (Array.isArray(obj)) return obj.length === 0
+  if (Object.prototype.toString.call(obj) === '[object Object]') {
+    return Object.keys(obj).length === 0
+  }
+  return false
 }

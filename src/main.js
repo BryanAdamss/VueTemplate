9b5cec8fd756ae19fae5fa1944c72bc28664a5f4
BryanAdamss <BryanAdamss@foxmail.com>
2018-10-14 00:13:36 +0800

    [fix] - 整合快捷调用相关方法的注册位置

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+
 import App from './App'
 import store from './store'
 import router from './router'
-import fastclick from 'fastclick'
-import VueLazyload from 'vue-lazyload'
-import axios from '@/common/js/axios'
 
-// * 绑定toast到Vue原型上，方便使用this.$toast来调用
 import BaseToast from '@/base/BaseToast/BaseToast'
+
+import axios from '@/common/js/axios'
+import { debounce, throttle } from '@/common/js/utils'
+
 Vue.use({
   install: Vue => {
+    // * 绑定去抖及节流函数到Vue原型上，方便直接调用
+    Vue.prototype.$debounce = debounce
+    Vue.prototype.$throttle = throttle
+    // * 绑定axios到Vue原型上，方便使用this.$http来调用接口
+    Vue.prototype.$http = axios
+    // * 绑定toast到Vue原型上，方便使用this.$toast来调用
     Vue.prototype.$toast = BaseToast
   }
 })
@@ -32,13 +41,6 @@ fastclick.attach(document.body)
 // 注册lazyload并配置默认图
 Vue.use(VueLazyload, {
   loading: require('common/img/loading.gif')
-})
-
-// * 绑定axios到Vue原型上，方便使用this.$http来调用接口
-Vue.use({
-  install: (Vue, options) => {
-    Vue.prototype.$http = axios
-  }
 })
 
 /* eslint-disable no-new */

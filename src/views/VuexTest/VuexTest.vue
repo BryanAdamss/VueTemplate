004c61393b2ba2eeba835a103d3621a403c1a0c1
BryanAdamss <BryanAdamss@foxmail.com>
2018-11-07 10:19:59 +0800

    [feat] - 使用createNamespacedHelpers

--- src/views/VuexTest/VuexTest.vue
+++ src/views/VuexTest/VuexTest.vue
@@ -20,7 +20,17 @@
  * * VuexTest
  */
 
-import { mapGetters, mapMutations } from 'vuex'
+import { createNamespacedHelpers } from 'vuex'
+
+const {
+  mapGetters: mapGettersForModuleA,
+  mapMutations: mapMutationsForModuleA
+} = createNamespacedHelpers('VuexTest/moduleA') // * 使用 createNamespacedHelpers 创建基于某个命名空间辅助函数
+
+const {
+  mapGetters: mapGettersForModuleB,
+  mapMutations: mapMutationsForModuleB
+} = createNamespacedHelpers('VuexTest/moduleB') // * 使用 createNamespacedHelpers 创建基于某个命名空间辅助函数
 
 import store from './store'
 
@@ -33,14 +43,18 @@ export default {
   },
   computed: {
     ...mapGetters(['globalTestObjName']), // 全局getter
-    ...mapGetters('VuexTest/moduleA', ['moduleANameLen']), // 模块a下的getter
-    ...mapGetters('VuexTest/moduleB', ['moduleBNameLen']) // 模块b下的getter
+    // ...mapGetters('VuexTest/moduleA', ['moduleANameLen']), // 使用传参形式获取模块a下的getter
+    ...mapGettersForModuleA(['moduleANameLen']), // 使用模块a专有mapGetter获取模块a下的getter
+    // ...mapGetters('VuexTest/moduleB', ['moduleBNameLen']), // 模块b下的getter
+    ...mapGettersForModuleB(['moduleBNameLen']) // 使用模块b专有mapGetter获取模块b下的getter
   },
   beforeRouteEnter(to, from, next) {
+    // 路由进入时注册模块
     store.install()
     next()
   },
   beforeRouteLeave(to, from, next) {
+    // 路由离开时卸载模块
     store.uninstall()
     next()
   },
@@ -51,13 +65,19 @@ export default {
       })
     },
     ...mapMutations({
-      setGlobalTestObj: 'SET_GLOBAL_TEST_OBJ'
+      setGlobalTestObj: 'SET_GLOBAL_TEST_OBJ' // 获取全局下的mutation
     }),
-    ...mapMutations('VuexTest/moduleA', {
-      setModuleAName: 'SET_MODULE_A_NAME'
+    // ...mapMutations('VuexTest/moduleA', {
+    //   setModuleAName: 'SET_MODULE_A_NAME'
+    // }),
+    ...mapMutationsForModuleA({
+      setModuleAName: 'SET_MODULE_A_NAME' // 使用模块a专有的mapMutation获取模块a的mutation
     }),
-    ...mapMutations('VuexTest/moduleB', {
-      setModuleBName: 'SET_MODULE_B_NAME'
+    // ...mapMutations('VuexTest/moduleB', {
+    //   setModuleBName: 'SET_MODULE_B_NAME'
+    // }),
+    ...mapMutationsForModuleB({
+      setModuleBName: 'SET_MODULE_B_NAME' // 使用模块b专有的mapMutation获取模块b的mutation
     })
   }
 }

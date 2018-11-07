e74b8a93995ef63638c70605641956c7975e28ad
BryanAdamss <BryanAdamss@foxmail.com>
2018-11-07 22:59:54 +0800

    [fix] - 修改vuex动态注册模块逻辑

--- src/views/VuexTest/VuexTest.vue
+++ src/views/VuexTest/VuexTest.vue
@@ -55,12 +55,18 @@ export default {
   },
   beforeRouteEnter(to, from, next) {
     // 路由进入时注册模块
-    store.install()
+    if (!window.installed) {
+      // 防止重复注册，vuex报错
+      store.install()
+      window.installed = true
+    }
     next()
   },
   beforeRouteLeave(to, from, next) {
-    // 路由离开时卸载模块
-    store.uninstall()
+    this.$once('hook:beforeDestroy', () => {
+      // 路由离开时卸载模块
+      store.uninstall() // uninstall的最佳时机应该在 DOM 更新中或后，旧的页面组件实例销毁过程调用时
+    })
     next()
   },
   methods: {

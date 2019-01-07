679ee4dd9e73163e4be54e0ddb450d84f88e4dc1
BryanAdamss <BryanAdamss@foxmail.com>
2019-01-07 19:42:15 +0800

    [add] - 添加eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,30 +6,39 @@ module.exports = {
     parser: 'babel-eslint'
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
+    // 'plugin:vue/essential',
+    'plugin:vue/recommended',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
+    // 关闭attribute强制横线连接
+    'vue/attribute-hyphenation': 'off',
+    // 设置缩进
+    indent: ['error', 2, { SwitchCase: 1 }],
+    // 关闭reject必须返回一个error
+    'prefer-promise-reject-errors': 'off',
+    // 允许同一个var声明多个变量
+    'one-var': 'off',
+    // 允许使用tab
+    'no-tabs': 'off',
+    // 允许函数前没有空格
+    'space-before-function-paren': 'off',
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    // 关闭indent检查
-    'indent': 'off',
-    // 关闭分号检查
-    'semi': 'off',
-    // 关闭函数前空格检查
-    'space-before-function-paren': 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  globals: {
+    // 指定全局变量并不允许改写
+    // ZXApp: false,
   }
 }
